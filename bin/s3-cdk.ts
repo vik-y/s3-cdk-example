33c500597126929e70b978bd5d6e6189b367a68a
vik-y <vikasyadavgood@gmail.com>
2022-01-12 11:22:43 +0530

    Raw and unclean but functional

--- bin/s3-cdk.ts
+++ bin/s3-cdk.ts
@@ -1,21 +1,25 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { S3CdkStack } from '../lib/s3-cdk-stack';
+import { S3CdkStack, S3Params } from '../lib/s3-cdk-stack';
 
-const app = new cdk.App();
-new S3CdkStack(app, 'S3CdkStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+import * as fs from 'fs'
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+const app = new cdk.App({
+  
 });
+
+
+// Read the params passed by kaholo plugin 
+const configData = JSON.parse(fs.readFileSync('params.json').toString())
+const  params: S3Params  = <S3Params>configData;
+
+
+// Generate env vars for CDK
+const env = {
+  region: params.REGION, 
+}
+
+new S3CdkStack(app, `${params.BUCKET_NAME}-stack`, {
+  env: env
+}, params);
--- lib/s3-cdk-stack.ts
+++ lib/s3-cdk-stack.ts
@@ -1,9 +1,58 @@
 import * as cdk from '@aws-cdk/core';
+import * as s3 from "@aws-cdk/aws-s3";
+import * as iam from '@aws-cdk/aws-iam';
+import { Duration, RemovalPolicy } from '@aws-cdk/core';
+
+export interface S3Params {
+  BUCKET_NAME: string;
+  REGION: string;
+  NEW_USER_NAME: string;
+  PUBLIC: boolean;
+  ACCESS_KEY_ID: string;
+  ACCESS_KEY_SECRET: string;
+  VERSIONED: boolean;
+}
+
 
 export class S3CdkStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps, kaholoParams?: S3Params) {
     super(scope, id, props);
 
+    if (kaholoParams){
+      const qualifier = `${kaholoParams.BUCKET_NAME}-kaholo`
     // The code that defines your stack goes here
+      
+
+      const bucket = new s3.Bucket(this, `${qualifier}-bucket`, {
+        bucketName: kaholoParams.BUCKET_NAME,
+        publicReadAccess: kaholoParams.PUBLIC,
+        versioned: kaholoParams.VERSIONED,
+        autoDeleteObjects: true, // For testing 
+        removalPolicy: RemovalPolicy.DESTROY, // For testing so we do cdk destroy to delete everything
+        
+        // An example of lifecycle policies. We can integrate these as params with plugins as needed
+        lifecycleRules: [{
+          expiration: Duration.days(365),
+          transitions: [{
+              storageClass: s3.StorageClass.INFREQUENT_ACCESS,
+              transitionAfter: Duration.days(30)
+          },{
+              storageClass: s3.StorageClass.GLACIER,
+              transitionAfter: Duration.days(90)
+          }]
+        }]
+      })
+
+      // Create a new user if requested in params
+      if(kaholoParams.NEW_USER_NAME){
+        const user = new iam.User(this, `${qualifier}-user`,{
+          userName: qualifier
+        })
+        bucket.grantReadWrite(user)
+      } 
+    }
+    else{
+      throw new Error("kaholoParams not present!"); 
+    }
   }
 }
